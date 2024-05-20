c3f768d69434d7b147a0ce7989198434912660c5
rendonir21@gmail.com <rendonir21@gmail.com>
2024-05-20 10:56:25 -0700

    second commit

--- functions.js
+++ functions.js
@@ -2,12 +2,43 @@
 
 // 1. isHometown
 
-// Define your function here
+function isHometown(town) {
+
+    return town === "San Francisco";
+}
+console.log(isHometown('San Francisco'));
+
 
 // 2. getFullName
-
-// Define your function here
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`
+}
+console.log(getFullName("Ivan", "Rendon"));
 
 // 3. calculateTotal
+function calculateTotal(basePrice, state, tax = 0.05) {
 
-// Define your function here
+    const subtotal = basePrice * (1 + tax);
+
+    let fee = 0;
+    if (state === "CA") {
+        fee = 0.03 * subtotal;
+    } else if (state === "PA") {
+        fee = 2;
+    } else if (state === "MA") {
+        if (basePrice <= 100 ) {
+            fee = 1;
+        } else {
+            fee = 3; 
+        }
+    }
+
+    return subtotal + fee;
+}
+
+console.log(calculateTotal(100, 'CA')); 
+console.log(calculateTotal(100, 'PA'));
+console.log(calculateTotal(100, 'MA')); 
+console.log(calculateTotal(150, 'MA')); 
+console.log(calculateTotal(100, 'TX'));
+
